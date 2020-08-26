a90557f23bc6e584a89ef70cce08de8f4b345fd8
alekseypetrenko <apetrenko.job@gmail.com>
2020-08-26 21:07:28 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <Todos 
+    v-bind:myPropsForBinding="todos"
+    v-on:del-todo="deleteTodo"
+    />
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos"
+
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  methods: {
+    deleteTodo(id){
+      this.todos = this.todos.filter(el => el.id !== id);
+    }  
+  },
+  data(){
+    return{
+      todos: [
+        {
+          id: 1,
+          title: "Todo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo 2",
+          completed: true
+        },
+
+        {
+          id: 3,
+          title: "Todo 3",
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.4;
+  }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <div v-for="todo in myPropsForBinding" v-bind:key="todo.id">
+            <TodoItem 
+            v-bind:todoItem="todo"
+            v-on:del-todo="$emit('del-todo', todo.id)"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+    name: "Todos",
+    props: ["myPropsForBinding"],
+    components: {
+        TodoItem
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
